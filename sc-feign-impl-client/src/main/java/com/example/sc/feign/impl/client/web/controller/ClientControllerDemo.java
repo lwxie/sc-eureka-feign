5d295e18b423c11921410cdad0217b309df7b569
longwei.xie <longwei.xie@topideal.com.cn>
2022-04-29 18:07:54 +0800

    feign接口统一异常处理。

--- sc-feign-impl-client/src/main/java/com/example/sc/feign/impl/client/web/controller/ClientControllerDemo.java
+++ sc-feign-impl-client/src/main/java/com/example/sc/feign/impl/client/web/controller/ClientControllerDemo.java
@@ -16,6 +16,7 @@ public class ClientControllerDemo {
     public String hiMhy(@RequestParam(name = "flag", required = false) int flag) {
         // 这里抛异常或者将Feign实现客户端关闭，即可实现熔断回退。
         if (flag == 1) {
+            // 注：如做了全局异常处理，那么当Feign实现客户端服务还存活着 异常处理走全局异常处理，如Feign实现客户端已然停止服务 异常处理走熔断回退。
             throw new ServiceException("sc:error_demo", "错误演示。");
         }
         return "Hello mhy!!!";
